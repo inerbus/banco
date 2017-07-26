5d99bebf175a361db611aac9874e118f5c1df892
alberto molina <alberto.molina1103@gmail.com>
2017-07-25 22:30:22 -0600

    Cuarto Commit, rep 1, agregando getter y setter Nit

--- Cliente.java
+++ Cliente.java
@@ -2,7 +2,7 @@ public class Cliente{
 	
 	private int idCliente;
 	private String nombreCliente;
-	private String Nit
+	private String Nit;
 	
 	public Cliente(){
 		
@@ -12,12 +12,18 @@ public class Cliente{
 		return this.idCliente;
 	}
 	public void setIdCliente(int idCliente){
-		this.idCliente=idCliente
+		this.idCliente=idCliente;
 	}
 	public String getNombreCliente(){
 		return this.nombreCliente;
 	}
 	public void setNombreCliente(String nombreCliente){
 		this.nombreCliente=nombreCliente;
+	}
+	public void setNit(String Nit){
+		this.Nit=Nit;
+	}
+	public String getNit(){
+		return this.Nit;
 	}
 }
